6b73f97550e5f6259c5161c9a5560e5b298c4167
InkyChew <B10623019@yuntech.edu.tw>
2019-09-16 07:48:54 +0800

    Updates

---
+++ precache-manifest.36500a421fb6f08adaccea7bda666751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "02497c27c70155ffd6d0",
+    "url": "js/chunk-vendors.e6f7c778.js"
+  },
+  {
+    "revision": "10006137507de31dce3e",
+    "url": "js/app.bb8c81c5.js"
+  },
+  {
+    "revision": "1e3597471e2e137a3679",
+    "url": "js/about.e4597025.js"
+  },
+  {
+    "revision": "f1889272a111895d8f6d228c2badde0d",
+    "url": "index.html"
+  },
+  {
+    "revision": "10006137507de31dce3e",
+    "url": "css/app.66015c03.css"
+  }
+];
